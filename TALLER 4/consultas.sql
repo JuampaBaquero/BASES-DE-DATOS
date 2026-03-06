7654c73cdc4cf77d11169ddc357c11dd7bd994fd
JuampaBaquero <jpablo-baquero@javeriana.edu.co>
2026-03-06 10:51:44 -0500

    Empecé a hacer las queries, revisenlas y ayudenme

--- TALLER 4/consultas.sql
+++ TALLER 4/consultas.sql
@@ -1,23 +1,61 @@
---Query 1:
+--Query 1
+SELECT 
+    cu.numero_cuenta "NUMERO CUENTA",
 
-SELECT cu.numero_cuenta CUENTA, 
-        SUM(
-            CASE
-                WHEN m.tipo IN ('R', 'D') THEN m.valor
-                WHEN m.tipo IN ('I', 'C') THEN -m.valor
-            END
-        ) SALDO,
-        COUNT(T.codigo_cliente) "Cantidad titulares",
-        o.nombre Oficina
+    NVL(
+        (SELECT SUM(valor)
+         FROM MOVIMIENTOS m
+         WHERE m.numero_cuenta = cu.numero_cuenta
+         AND m.tipo IN ('R','D')
+        ),0)
+    -
+    NVL(
+        (SELECT SUM(valor)
+         FROM MOVIMIENTOS m
+         WHERE m.numero_cuenta = cu.numero_cuenta
+         AND m.tipo IN ('I','C')
+        ),0) SALDO,
+    (
+        SELECT COUNT(*)
+        FROM TITULARES t
+        WHERE t.numero_cuenta = cu.numero_cuenta
+    ) "CANTIDAD TITULARES",
+    o.nombre OFICINA
 
 FROM CUENTAS cu
-    INNER JOIN MOVIMIENTOS m
-        ON m.numero_cuenta = cu.numero_cuenta
-
-    INNER JOIN TITULARES t
-        ON t.numero_cuenta = cu.numero_cuenta
-
     INNER JOIN OFICINAS o
         ON o.codigo_oficina = cu.codigo_oficina
+ORDER BY cu.numero_cuenta;
 
-GROUP BY cu.numero_cuenta, o.nombre;
+--Query 2
+
+--Debería hacer una vista temporal para obtener cosas
+
+WITH GENEROXOFICINA AS(
+    SELECT 
+        o.nombre NOMBRE_OFICINA,
+        c.genero GENERO,
+        m.valor VALOR
+
+
+    FROM OFICINAS o
+
+        LEFT JOIN CUENTAS cu
+            ON cu.codigo_oficina = o.codigo_oficina
+
+        INNER JOIN MOVIMIENTOS m
+            ON m.numero_cuenta = cu.numero_cuenta
+        
+        INNER JOIN TITULARES t
+            ON t.numero_cuenta = cu.numero_cuenta
+
+        INNER JOIN CLIENTES c
+            ON t.codigo_cliente = c.codigo_cliente
+    GROUP BY c.genero, m.valor
+)
+
+SELECT 
+    
+
+
+
