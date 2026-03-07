7cfef5c7fc7fd942925ed004b87d1161cc0cac9b
JuampaBaquero <jpablo-baquero@javeriana.edu.co>
2026-03-07 10:47:43 -0500

    Sofía revisa porfa la segunda Query

--- TALLER 4/consultas.sql
+++ TALLER 4/consultas.sql
@@ -29,30 +29,103 @@ ORDER BY cu.numero_cuenta;
 
 --Query 2
 
---Debería hacer una vista temporal para obtener cosas
-
-WITH GENEROXOFICINA AS(
+WITH DATOS AS (
     SELECT 
-        o.nombre NOMBRE_OFICINA,
-        c.genero GENERO,
-        m.valor VALOR
-
-
+        o.codigo_oficina,
+        o.nombre nombre_oficina,
+        m.tipo,
+        m.valor,
+        m.numero,
+        m.fecha_movimiento,
+        c.genero,
+        c.codigo_cliente
     FROM OFICINAS o
 
         LEFT JOIN CUENTAS cu
             ON cu.codigo_oficina = o.codigo_oficina
 
-        INNER JOIN MOVIMIENTOS m
+        LEFT JOIN MOVIMIENTOS m
             ON m.numero_cuenta = cu.numero_cuenta
-        
-        INNER JOIN TITULARES t
+
+        LEFT JOIN TITULARES t
             ON t.numero_cuenta = cu.numero_cuenta
 
-        INNER JOIN CLIENTES c
-            ON t.codigo_cliente = c.codigo_cliente
-    GROUP BY c.genero, m.valor
+        LEFT JOIN CLIENTES c
+            ON c.codigo_cliente = t.codigo_cliente
 )
+
+SELECT
+    o.nombre "OFICINA",
+
+    (
+        SELECT COUNT(DISTINCT d.codigo_cliente)
+        FROM DATOS d
+        WHERE d.codigo_oficina = o.codigo_oficina
+        AND d.genero = 'M'
+    ) "NÚMERO DE CLIENTES HOMBRES",
+
+    (
+        SELECT COUNT(DISTINCT d.codigo_cliente)
+        FROM DATOS d
+        WHERE d.codigo_oficina = o.codigo_oficina
+        AND d.genero = 'F'
+    ) "NÚMERO DE CLIENTES MUJERES",
+    NVL(
+    (
+        SELECT AVG(
+            (SELECT SUM(d1.valor)
+             FROM DATOS d1
+             WHERE d1.codigo_oficina = o.codigo_oficina
+             AND d1.genero='F'
+             AND d1.tipo IN ('R','D'))
+            -
+            (SELECT SUM(d2.valor)
+             FROM DATOS d2
+             WHERE d2.codigo_oficina = o.codigo_oficina
+             AND d2.genero='F'
+             AND d2.tipo IN ('I','C'))
+        )
+        FROM DATOS
+    ), 0) "PROMEDIO SALDOS MUJERES",
+
+    NVL(
+    (
+        SELECT AVG(
+            (SELECT SUM(d1.valor)
+             FROM DATOS d1
+             WHERE d1.codigo_oficina = o.codigo_oficina
+             AND d1.genero='M'
+             AND d1.tipo IN ('R','D'))
+            -
+            (SELECT SUM(d2.valor)
+             FROM DATOS d2
+             WHERE d2.codigo_oficina = o.codigo_oficina
+             AND d2.genero='M'
+             AND d2.tipo IN ('I','C'))
+        )
+        FROM DATOS
+    ), 0) "PROMEDIO SALDOS HOMBRES",
+
+    (
+        SELECT MAX(d.fecha_movimiento)
+        FROM DATOS d
+        WHERE d.codigo_oficina = o.codigo_oficina
+    ) "FECHA ÚLTIMO MOVIMIENTO",
+
+    (
+        SELECT MIN(d.fecha_movimiento)
+        FROM DATOS d
+        WHERE d.codigo_oficina = o.codigo_oficina
+    ) "FECHA PRIMER MOVIMIENTO",
+
+    (
+        SELECT COUNT(DISTINCT d.numero)
+        FROM DATOS d
+    ) "CANTIDAD HISTÓRICA DE MOVIMIENTOS"
+
+FROM OFICINAS o
+
+ORDER BY o.nombre;
     
 --query 3
 SELECT 
