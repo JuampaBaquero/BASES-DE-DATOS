6d3d324b84a83179a5ec963f6d842a61a93dd731
JuampaBaquero <jpablo-baquero@javeriana.edu.co>
2026-03-21 23:04:17 -0500

    ya hay inserts y crear tablas de Soph

---
+++ proyecto/Entrega 1/queries.sql
@@ -0,0 +1,12 @@
+SELECT * FROM PAIS;
+
+
+
+--1:
+--2: 
+--3:
+--4: Goles de cada selecicon en todo el torneo 
+--5: Cual es el porcentaje de particiapntes de cada eleccion en el total de goles 
+--6: que selecciones han jugado en todas las ciudades
+--7:
+--8:
